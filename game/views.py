211599d3830f764f8c3c78188192bc60e2eb99fc
cjyasleep <779689450@qq.com>
2023-03-10 18:10:45 +0800

    add picture

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src ="https://img2.baidu.com/it/u=1053443276,127426840&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500" width=2000>'
+    return HttpResponse(line1+line2)
